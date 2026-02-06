9f0411af65e0eccf00caac7d8407b416a67a9a65
Vaidehi Mujumdar <vaidehimujumdar@Vaidehis-MacBook-Air.local>
2026-02-07 00:39:16 +0530

    style: increase logo size

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -101,9 +101,9 @@ export function SiteHeader() {
                         <Image
                             src="/logo-new.jpg"
                             alt="Clean Crate"
-                            width={160}
-                            height={40}
-                            className="h-10 w-auto object-contain"
+                            width={180}
+                            height={60}
+                            className="h-16 w-auto object-contain"
                             priority
                         />
                     </Link>
